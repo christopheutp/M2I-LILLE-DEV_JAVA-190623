06afeaeef1a8dfe5136eba488db2ea04ec1d9433
christopheM2i <christophe.delory@labom2iformation.fr>
2023-08-16 14:56:23 +0200

    debut demo jdbc

---
+++ java_JDBC/demo__JDBC/src/main/java/org/example/Main.java
@@ -0,0 +1,31 @@
+package org.example;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // test de connexion à la BDD
+        String url = "jdbc:postgresql://localhost:5432/demo_jdbc";
+        String user = "postgres";
+        String password = "Pa$$w0rd";
+
+    try {
+        Connection connection = DriverManager.getConnection(url,user,password);
+        if(connection != null){
+            System.out.println("La connexion est ok");
+        }else {
+            System.out.println("connexion echoué");
+        }
+
+
+    }catch (SQLException e){
+        throw new RuntimeException(e);
+    }
+
+
+    }
+}
