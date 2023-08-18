83be03e8f2e8c96bbcf1f7c564c9383de3f68302
christopheM2i <christophe.delory@labom2iformation.fr>
2023-08-18 08:53:52 +0200

    dossier renomme

---
+++ java_JDBC/demo_DAO_JDBC/src/main/java/org/example/model/Person.java
@@ -0,0 +1,52 @@
+package org.example.model;
+
+public class Person {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public Person(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
