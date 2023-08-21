27f84c7cb0fca529267a0ce4fae9a6198a71aca8
christopheM2i <christophe.delory@labom2iformation.fr>
2023-08-21 15:16:36 +0200

    demo jpa

--- java_JPA/demo_jpa/src/main/java/org/example/Main.java
+++ java_JPA/demo_jpa/src/main/java/org/example/Main.java
@@ -5,6 +5,9 @@ import org.example.demo.Demo1;
 
 public class Main {
     public static void main(String[] args) {
-        Demo1.main();
+       // Demo1.main();
+       // Demo1.main2();
+       // Demo1.remove();
+        Demo1.createQuery();
     }
 }
--- java_JPA/demo_jpa/src/main/java/org/example/demo/Demo1.java
+++ java_JPA/demo_jpa/src/main/java/org/example/demo/Demo1.java
@@ -5,6 +5,7 @@ import org.example.entity.Person;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class Demo1 {
 
@@ -16,7 +17,7 @@ public class Demo1 {
 
         em.getTransaction().begin();
 
-        Person person = new Person("Delory","Christophe");
+        Person person = new Person("toto","tata");
         System.out.println("ID de la personne avant persist : "+person.getId());
         em.persist(person);
         System.out.println("ID de la personne aprés persist : "+person.getId());
@@ -25,4 +26,49 @@ public class Demo1 {
         emf.close();
 
     }
+
+    public static void main2(){
+        EntityManager em = emf.createEntityManager();
+
+        Person person = em.find(Person.class, 42L);
+        System.out.println(person.toString());
+        em.close();
+        emf.close();
+
+    }
+
+    public static void remove(){
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+
+        Person person = em.find(Person.class, 1L);
+        em.remove(person);
+
+        em.getTransaction().commit();
+        em.close();
+        emf.close();
+    }
+
+    public static void createQuery(){
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+
+        Person person = new Person("Delory","Christophe");
+        Person person1 = new Person("Toto","Tata");
+
+        em.persist(person);
+        em.persist(person1);
+
+        List<Person> personList = null;
+
+        personList = em.createQuery("select p from Person p", Person.class).getResultList();
+
+        for (Person p:personList){
+            System.out.println(p);
+        }
+
+        em.getTransaction().commit();
+
+    }
+
 }
