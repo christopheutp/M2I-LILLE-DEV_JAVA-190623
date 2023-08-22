ddc0427beb684062dd528db2fe76a5999fa0df76
christopheM2i <christophe.delory@labom2iformation.fr>
2023-08-22 14:55:57 +0200

    demo jpa suite

--- java_JPA/demo_jpa/src/main/java/org/example/Main.java
+++ java_JPA/demo_jpa/src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 
 import org.example.demo.Demo1;
 import org.example.demo.Demo2;
+import org.example.demo.Demo3;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,6 +12,7 @@ public class Main {
        // Demo1.main3();
        // Demo1.remove();
        // Demo1.createQuery();
-        Demo2.main();
+       // Demo2.main();
+        Demo3.main();
     }
 }
--- java_JPA/demo_jpa/src/main/java/org/example/demo/Demo3.java
+++ java_JPA/demo_jpa/src/main/java/org/example/demo/Demo3.java
@@ -1,7 +1,36 @@
 package org.example.demo;
 
-public class Demo3 {
-    public static void main() {
+import org.example.entity.oneToOne.Adress;
+import org.example.entity.oneToOne.House;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Demo3 {
+    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa_demo_postgres");
+    public static void main() {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+
+        Adress adress = new Adress();
+        adress.setNumero(46);
+        adress.setNomRue("rue des pias");
+        adress.setCodePostal("59000");
+        adress.setVille("Lille");
+
+
+        House house = new House();
+
+        house.setTaille(432);
+        house.setAdress(adress);
+        em.persist(house);
+
+        em.getTransaction().commit();
+        House housesearch = em.find(House.class, house.getId());
+        System.out.println(housesearch);
+
+        em.close();
+        emf.close();
     }
 }
