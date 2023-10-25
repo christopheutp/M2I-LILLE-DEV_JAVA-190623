47ff46ebc8bf07843d29251fc2369e9127687a04
christopheutp <test@example.com>
2023-10-25 11:55:10 +0200

    Demo requêtes

---
+++ javascript/bases_requetes/assets/js/Post.js
@@ -0,0 +1,10 @@
+class Post {
+    constructor(userId, id, title, body) {
+        this.userId = userId
+        this.id = id
+        this.title = title
+        this.body = body
+    }
+}
+
+export default Post
---
+++ javascript/bases_requetes/assets/js/main.mjs
@@ -0,0 +1,67 @@
+import Post from "./Post.js"
+
+const url = "https://jsonplaceholder.typicode.com/posts"
+
+// fetch(url)
+// .then(result => {
+//     return result.json()
+// })
+// .then(data => {
+//     console.log(data[4].body);
+// })
+
+const oneLiner = await (await fetch(url)).json()
+
+let response
+let data
+
+try {
+    response = await fetch(url)
+    console.log(response);
+} catch(err) {
+    console.error(err);
+}
+
+try {
+    data = await response.json()
+} catch(err) {
+    console.error(err);
+}
+
+console.log(data[4].body);
+console.log(oneLiner[4].body);
+
+const newPost = new Post(
+    2,
+    47,
+    "Title A",
+    "Body A"
+)
+
+try {
+    response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(newPost)
+    })
+
+    console.log(await response.json());
+} catch (err) {
+    console.error(err);
+}
+
+
+try {
+    response = await fetch("http://localhost:8080/api/v1/demo")
+    const data = await response.json()
+    console.log(data);
+} catch (err) {
+    console.error(err);
+}
+
+try {
+    response = await fetch("http://localhost:8080/api/v1/demo/other")
+    const data = await response.json()
+    console.log(data);
+} catch (err) {
+    console.error(err);
+}
