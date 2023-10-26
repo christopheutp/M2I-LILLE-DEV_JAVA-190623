b42e24a3e4f73b24aa9723a3a663ca91c0da4694
christopheutp <test@example.com>
2023-10-26 12:28:19 +0200

    Recap des bases en Ts

---
+++ typescript/demo_bases_typescript/src/assets/js/recap.js
@@ -0,0 +1,55 @@
+"use strict";
+let unAge = 25;
+if (unAge >= 21) {
+    console.log("Il est majeur aux USA");
+}
+else if (unAge >= 18) {
+    console.log("Il est majeur en France");
+}
+else {
+    console.log("Il est mineur en France et aux USA");
+}
+let laReponse = "A";
+switch (laReponse) {
+    case "A":
+        console.log("Vous avez choisi A");
+        break;
+    case "B":
+        console.log("Vous avez choisi B");
+        break;
+    case "C":
+        console.log("Vous avez choisi C");
+        break;
+    case "D":
+        console.log("Vous avez choisi D");
+        break;
+    default:
+        console.log("je n'ai pas compris");
+        break;
+}
+for (let i = 0; i < 10; i++) {
+    console.log("Je me répète");
+}
+const desPrenoms = ["John", "Michel", "Albert"];
+for (let index = 0; index < desPrenoms.length; index++) {
+    const element = desPrenoms[index];
+    console.log(element);
+}
+for (const pre of desPrenoms) {
+    console.log(pre);
+}
+function mafonction() {
+    console.log("mafonction est lancée !");
+}
+mafonction();
+function monAddition(nbA, nbB) {
+    return nbA + nbB;
+}
+const monAdditionVariable = (nbA, nbB) => {
+    return nbA + nbB;
+};
+const monOperation = (nbA, nbB, quelCalcul) => {
+    console.log("Je fais un calcul");
+    return quelCalcul(nbA, nbB);
+};
+const unResultat = monOperation(10, 2, monAdditionVariable);
