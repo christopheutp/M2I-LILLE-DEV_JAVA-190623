0b5620117c8cec33e2ae71f199e23ade42e33614
christopheutp <test@example.com>
2023-11-06 10:53:40 +0100

    ajout support

---
+++ angular/exercices/exercice-10/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { interval } from 'rxjs'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'exercice-10';
+}
