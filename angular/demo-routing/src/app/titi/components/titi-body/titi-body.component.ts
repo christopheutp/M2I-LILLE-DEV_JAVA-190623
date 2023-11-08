5cf0c8583e0b82268944aa8adcc967ced262d690
christopheutp <test@example.com>
2023-11-08 11:45:17 +0100

    Demo Routing + PreConf

---
+++ angular/demo-routing/src/app/titi/components/titi-body/titi-body.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-titi-body',
+  templateUrl: './titi-body.component.html',
+  styleUrls: ['./titi-body.component.css']
+})
+export class TitiBodyComponent {
+
+}
